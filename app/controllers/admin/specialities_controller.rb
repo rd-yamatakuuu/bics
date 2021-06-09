25691b692dcfbb27efd169e32afb424d7f6d1963
rd-yamatakuuu <orgchem.techac@gmail.com>
2021-06-09 07:29:07 +0000

    [admin]MVC of tag, speciality

--- app/controllers/admin/specialities_controller.rb
+++ app/controllers/admin/specialities_controller.rb
@@ -1,2 +1,42 @@
 class Admin::SpecialitiesController < ApplicationController
+
+  def index
+    @specialities = Speciality.page(params[:page]).per(10)
+    @speciality = Speciality.new
+  end
+
+  def edit
+    @speciality = Speciality.find(params[:id])
+  end
+
+  def create
+    @speciality = Speciality.new(speciality_params)
+    if @speciality.save
+      redirect_to admin_specialities_path
+    else
+      render :index
+    end
+  end
+
+  def update
+    @speciality = Speciality.find(params[:id])
+    if @speciality.update(speciality_params)
+      redirect_to admin_specialities_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @speciality = Speciality.find(params[:id])
+    @speciality.destroy
+    redirect_to admin_specialities_path
+  end
+
+  private
+
+  def speciality_params
+    params.require(:speciality).permit(:name)
+  end
+
 end
--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,2 +1,42 @@
 class Admin::TagsController < ApplicationController
+  
+  def index
+    @tags = Tag.page(params[:page]).per(10)
+    @tag = Tag.new
+  end
+  
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+  
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to admin_tags_path
+    else
+      render :index
+    end
+  end
+  
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      redirect_to admin_tags_path
+    else
+      render :index
+    end
+  end
+  
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to admin_tags_path
+  end
+  
+  private
+  
+  def tag_params
+    params.require(:tag).permit(:tag_name)
+  end
+  
 end

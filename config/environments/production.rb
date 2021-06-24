c95c093bf0a6df5f0a5b94966dd19b7df8989eb2
rd-yamatakuuu <orgchem.techac@gmail.com>
2021-06-24 08:13:51 +0000

    [public]modification contact

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      port: 587,
+      address: 'smtp.gmail.com',
+      domain: 'gmail.com',
+      user_name: ENV['CONTACT_USERNAME'],
+      password: ENV['CONTACT_PASSWORD'],
+      authentication: "plain",
+      enable_starttls_auto: true
+  }
 end

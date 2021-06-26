85f556f6ab8cd8447efc44bece61a59a70a10dfd
rd-yamatakuuu <orgchem.techac@gmail.com>
2021-06-26 16:39:23 +0000

    [public]modification favorite

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,28 +1,29 @@
 class Public::FavoritesController < ApplicationController
-  
+
   before_action :authenticate_user!
 
   def create
     @idea = Idea.find(params[:idea_id])
-    @favorite = Favorite.new(idea_id: @idea.id)
-    @favorite.user_id = current_user.id
+    # @favorite = Favorite.new(idea_id: @idea.id)
+    # @favorite.user_id = current_user.id
+    @favorite = current_user.favorites.new(idea_id: @idea.id)
     @favorite.save
-    
     @idea.create_notification_by(current_user)
-  
+
   end
 
   def destroy
     @idea = Idea.find(params[:idea_id])
-    @favorite = Favorite.find_by(idea_id: @idea.id)
-    @favorite.user_id = current_user.id
+    # @favorite = Favorite.find_by(idea_id: @idea.id)
+    # @favorite.user_id = current_user.id
+    @favorite = current_user.favorites.find_by(idea_id: @idea.id)
     @favorite.destroy
   end
-  
+
   def index
     @user = current_user
     @favorites = @user.favorites
   end
-  
-  
+
+
 end
